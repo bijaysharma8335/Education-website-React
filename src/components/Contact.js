a719cbecf4ab6fb4c2606ef9c36eb3e186e6bac3
Bijay Sharma <bijaysharma8335@gmail.com>
2023-01-31 19:31:29 +0530

    Social media links inContact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -40,7 +40,7 @@ const Contact = () => {
                     <h5>Connect With Us</h5>
                     <ul className="contact_socials">
                         <li>
-                            <Link to="/">
+                            <a href="https://www.facebook.com/bijay.sharma.8335">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="18"
@@ -51,12 +51,12 @@ const Contact = () => {
                                 >
                                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                                 </svg>
-                            </Link>
+                            </a>
                         </li>
                         <li>
-                            <Link to="/">
+                            <a href="https://www.instagram.com/bijaysharma5311/">
                                 <i className="uil uil-instagram-alt"></i>
-                            </Link>
+                            </a>
                         </li>
                         <li>
                             <Link to="/">
@@ -64,9 +64,9 @@ const Contact = () => {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/">
+                            <a href='https://www.linkedin.com/in/bijay-sharma-49b64a176/'>
                                 <i className="uil uil-linkedin-alt"></i>
-                            </Link>
+                            </a>
                         </li>
                     </ul>
                 </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+import { Image } from "react-bootstrap";
 import Category from "./Category";
 
 import Faq from "./Faq";
@@ -54,27 +55,34 @@ const Home = () => {
                 touchTracking={false}
                 disableButtonsControls
                 className="mt-5"
+                // responsiveness={
+                //     {
+                //         0: {
+                //             items: 1,
+                //         },
+                //         1024: {
+                //             items: 3
+                //         }
+                //       }
+                // }
             />
-            <Container>
-                <div className="header">
-                    <div className="header_container">
-                        <div className="header_left">
-                            <h1>
-                                Grow your skills to advance your career growth
-                            </h1>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Recusandae reprehenderit quod
-                                adipisci qui
-                            </p>
-                            <Link
-                                to="/courses"
-                                className="button button-primary text-decoration-none"
-                            >
-                                Get Started
-                            </Link>
-                        </div>
-                        {/* <div className="header_right-image">
+            {/* <Container> */}
+            <div className="header">
+                <div className="header_container">
+                    <div className="header_left">
+                        <h1>Grow your skills to advance your career growth</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Recusandae reprehenderit quod adipisci qui
+                        </p>
+                        <Link
+                            to="/courses"
+                            className="button button-primary text-decoration-none"
+                        >
+                            Get Started
+                        </Link>
+                    </div>
+                    {/* <div className="header_right-image">
                             <div className="header_right">
                                 <img
                                     src="https://img.freepik.com/premium-vector/education-technology-page-smartphone-page_316839-2330.jpg?w=2000"
@@ -82,44 +90,47 @@ const Home = () => {
                                 />
                             </div>
                         </div> */}
-                    </div>
                 </div>
-                <Category />
-                <h2 className="text-center my-5"> Our Popular Courses</h2>
-                <div className="container courses_container mt-2">
-                    {courses.map((course, i) => {
-                        if (i < 3) {
-                            return (
-                                <div
-                                    className="course"
-                                    key={i}
-                                    // onClick={() => navigate("/courses")}
-                                >
-                                    <img
-                                        src={course.image}
-                                        alt=""
-                                        height={200}
-                                    />
+            </div>
+            <Category />
+            <h2 className="text-center my-5"> Our Popular Courses</h2>
+            <div className="container courses_container mt-2">
+                {courses.map((course, i) => {
+                    if (i < 3) {
+                        return (
+                            <div
+                                className="course"
+                                key={i}
+                                // onClick={() => navigate("/courses")}
+                            >
+                                <img
+                                    fluid={true}
+                                    src={course.image}
+                                    alt=""
+                                    height={200}
+                                    style={{ objectFit: "contain !important" }}
+                                />
 
-                                    <div className="course_info">
-                                        <h4>{course.title}</h4>
-                                        <p>{course.description}</p>
-                                        <Link
-                                            to="/courses"
-                                            state={{ course }}
-                                            className="button button-primary text-decoration-none"
-                                        >
-                                            Learn More
-                                        </Link>
-                                    </div>
+
+                                <div className="course_info">
+                                    <h4>{course.title}</h4>
+                                    <p>{course.description}</p>
+                                    <Link
+                                        to="/courses"
+                                        state={{ course }}
+                                        className="button button-primary text-decoration-none"
+                                    >
+                                        Learn More
+                                    </Link>
                                 </div>
-                            );
-                        }
-                    })}
-                </div>
-                <Faq />
-                <Testimonials />
-            </Container>
+                            </div>
+                        );
+                    }
+                })}
+            </div>
+            <Faq />
+            <Testimonials />
+            {/* </Container> */}
         </>
     );
 };
